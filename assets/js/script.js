6ca984b77c75d6f4a4dd250d8a15b26ddabf94a1
katiullahaxioned <atiullahk@axioned.com>
2022-08-27 13:07:50 +0530

    preventDefault added for modalContent

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,32 +6,40 @@ var modalContent = document.querySelector(".modal-content");
 var modalImage = document.querySelector(".modal-content img");
 var modalTitle = document.querySelector(".modal-content .card-title");
 
+function commonForEach(list) {
+  var cardImageSrc = list.querySelector(".card img").getAttribute("src");
+  var cardTitleText = list.querySelector(".card .card-title").innerText;
+
+  modalTitle.innerText = cardTitleText;
+  modalImage.setAttribute("src", cardImageSrc);
+  html.classList.add("overflow-hidden");
+  modal.classList.add("active-block");
+  closeButtton.classList.add("active-block");
+}
+
+function commonClose(e) {
+  e.stopPropagation();
+  modalTitle.innerHTML = "";
+  modalImage.setAttribute("src", "");
+  html.classList.remove("overflow-hidden");
+  modal.classList.remove("active-block");
+  closeButtton.classList.remove("active-block");
+}
+
 card.forEach(function (list) {
   list.addEventListener("click", function () {
-    var cardImageSrc = list.querySelector(".card img").getAttribute("src");
-    var cardTitleText = list.querySelector(".card .card-title").innerText;
-
-    modalTitle.innerHTML = cardTitleText;
-    modalImage.setAttribute("src", cardImageSrc);
-
-    html.classList.add("overflow");
-    modal.classList.add("active");
-    closeButtton.classList.add("active");
-
-    closeButtton.addEventListener("click", function () {
-      modalTitle.innerHTML = "";
-      modalImage.setAttribute("src", "");
-      html.classList.remove("overflow");
-      modal.classList.remove("active");
-      closeButtton.classList.remove("active");
-    });
-
-    modal.addEventListener("click", function () {
-      modalTitle.innerHTML = "";
-      modalImage.setAttribute("src", "");
-      html.classList.remove("overflow");
-      modal.classList.remove("active");
-      closeButtton.classList.remove("active");
-    });
+    commonForEach(list);
   });
+});
+
+closeButtton.addEventListener("click", function (e) {
+  commonClose(e);
+});
+
+modal.addEventListener("click", function (e) {
+  commonClose(e);
+});
+
+modalContent.addEventListener("click", function (e) {
+  e.stopPropagation();
 });
